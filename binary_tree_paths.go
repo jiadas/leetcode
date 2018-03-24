3112284bc4ce41dbd2c745ed6d1b51b084adf6c5
jia <jiada8866@126.com>
2018-03-25 02:05:54 +0800

    feat(solution): Binary Tree Paths

---
+++ binary_tree_paths.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+//Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func binaryTreePaths(root *TreeNode) []string {
+	if root == nil {
+		return nil
+	}
+
+	return dfsTree(root, "", nil)
+}
+
+func dfsTree(node *TreeNode, path string, paths []string) []string {
+	if path != "" {
+		path = fmt.Sprintf("%s->%d", path, node.Val)
+	} else {
+		path = fmt.Sprintf("%d", node.Val)
+	}
+	if node.Left == nil && node.Right == nil {
+		return append(paths, path)
+	}
+
+	var l []string
+	if node.Left != nil {
+		paths = append(paths, dfsTree(node.Left, path, l)...)
+	}
+	var r []string
+	if node.Right != nil {
+		paths = append(paths, dfsTree(node.Right, path, r)...)
+	}
+	return paths
+}
+
+func main() {
+	fmt.Println(binaryTreePaths(&TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val: 2,
+		},
+		Right: &TreeNode{
+			Val: 3,
+		},
+	}))
+}
