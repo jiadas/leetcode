eedb010e64506c2a07a111d5351abc682f7bc916
shijiada <jiada8866@126.com>
2018-03-28 18:00:32 +0800

    feat(solution): Permutations

---
+++ permutations.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func permute(nums []int) [][]int {
+	return doPermute(nums, nil, nil)
+}
+
+func doPermute(nums []int, p []int, result [][]int) [][]int {
+	if len(nums) == 0 {
+		return append(result, p)
+	}
+	for i, value := range nums {
+		r := append([]int{}, nums[:i]...)
+		r = append(r, nums[i+1:]...)
+		result = append(result, doPermute(r, append(p, value), nil)...)
+	}
+
+	return result
+}
+
+func main() {
+	fmt.Println(permute([]int{1, 2, 3}))
+	fmt.Println(permute([]int{1, 2}))
+	fmt.Println(permute([]int{}))
+}
