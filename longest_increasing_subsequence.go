4e57fe15659f4ea1cf99c5b36845ad2356ffb8f5
jia <jiada8866@126.com>
2018-04-04 00:35:16 +0800

    feat(solution): 300. Longest Increasing Subsequence

---
+++ longest_increasing_subsequence.go
@@ -0,0 +1,65 @@
+package main
+
+import "fmt"
+
+func lengthOfLIS(nums []int) int {
+	lis := make([]int, len(nums))
+	for i := 0; i < len(nums); i++ {
+		max := 1
+		for j := 0; j < i; j++ {
+			if nums[i] > nums[j] {
+				if lis[j]+1 > max {
+					max = lis[j] + 1
+				}
+			}
+		}
+		lis[i] = max
+	}
+	var result int
+	for _, value := range lis {
+		if value > result {
+			result = value
+		}
+	}
+	return result
+}
+
+// tails is an array storing the smallest tail of all increasing subsequences with length i+1 in tails[i].
+// For example, say we have nums = [4,5,6,3], then all the available increasing subsequences are:
+//
+// len = 1   :      [4], [5], [6], [3]   => tails[0] = 3
+// len = 2   :      [4, 5], [5, 6]       => tails[1] = 5
+// len = 3   :      [4, 5, 6]            => tails[2] = 6
+// We can easily prove that tails is a increasing array. Therefore it is possible to do a binary search in tails array to find the one needs update.
+//
+// Each time we only do one of the two:
+//
+// (1) if x is larger than all tails, append it, increase the size by 1
+// (2) if tails[i-1] < x <= tails[i], update tails[i]
+// Doing so will maintain the tails invariant. The the final answer is just the size.
+func lengthOfLISBinary(nums []int) int {
+	tails := make([]int, len(nums))
+	var size int
+	for _, value := range nums {
+		l, h := 0, size
+		for l < h {
+			m := l + (h-l)/2
+			if tails[m] >= value {
+				h = m
+			} else {
+				l = m + 1
+			}
+		}
+		tails[l] = value
+		if l == size {
+			size++
+		}
+	}
+	return size
+}
+
+func main() {
+	fmt.Println(lengthOfLIS([]int{10, 9, 2, 5, 3, 7, 101, 18}))
+	fmt.Println(lengthOfLISBinary([]int{10, 9, 2, 5, 3, 7, 101, 18}))
+	fmt.Println(lengthOfLISBinary([]int{2, 2}))
+}
