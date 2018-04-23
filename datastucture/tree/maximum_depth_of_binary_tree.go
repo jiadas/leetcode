0bd3e0eb70ba8668d4541ed1e852690fecd4fae3
jia <jiada8866@126.com>
2018-04-23 23:08:59 +0800

    feat(solution): 104. Maximum Depth of Binary Tree

---
+++ datastucture/tree/maximum_depth_of_binary_tree.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+}
+
+func max(a, b int) int {
+	if a >= b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	root := &TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val: 2,
+		},
+		Right: &TreeNode{
+			Val: 3,
+		},
+	}
+	fmt.Println(maxDepth(root))
+}
