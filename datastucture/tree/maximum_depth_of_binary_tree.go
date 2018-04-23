1b4b381eb948c6df388a1009593d21c87b9ecbfb
jia <jiada8866@126.com>
2018-04-24 00:22:45 +0800

    feat(solution): 617. Merge Two Binary Trees

--- datastucture/tree/maximum_depth_of_binary_tree.go
+++ datastucture/tree/maximum_depth_of_binary_tree.go
@@ -1,21 +1,5 @@
 package main
 
-import "fmt"
-
-/**
- * Definition for a binary tree node.
- * type TreeNode struct {
- *     Val int
- *     Left *TreeNode
- *     Right *TreeNode
- * }
- */
-type TreeNode struct {
-	Val   int
-	Left  *TreeNode
-	Right *TreeNode
-}
-
 func maxDepth(root *TreeNode) int {
 	if root == nil {
 		return 0
@@ -28,17 +12,4 @@ func max(a, b int) int {
 		return a
 	}
 	return b
-}
-
-func main() {
-	root := &TreeNode{
-		Val: 1,
-		Left: &TreeNode{
-			Val: 2,
-		},
-		Right: &TreeNode{
-			Val: 3,
-		},
-	}
-	fmt.Println(maxDepth(root))
 }
---
+++ datastucture/tree/merge_two_binary_trees.go
@@ -0,0 +1,31 @@
+package main
+
+func mergeTrees(t1 *TreeNode, t2 *TreeNode) *TreeNode {
+	if t1 == nil && t2 == nil {
+		return nil
+	}
+
+	// 这样声明的 head 默认值是 nil，后续操作 head 里的成员会 panic
+	// var head *TreeNode
+	head := &TreeNode{}
+	var left1, left2 *TreeNode
+	var right1, right2 *TreeNode
+	switch {
+	case t1 != nil && t2 != nil:
+		head.Val = t1.Val + t2.Val
+		left1, left2 = t1.Left, t2.Left
+		right1, right2 = t1.Right, t2.Right
+	case t1 != nil && t2 == nil:
+		head.Val = t1.Val
+		left1 = t1.Left
+		right1 = t1.Right
+	case t1 == nil && t2 != nil:
+		head.Val = t2.Val
+		left2 = t2.Left
+		right2 = t2.Right
+	}
+
+	head.Left = mergeTrees(left1, left2)
+	head.Right = mergeTrees(right1, right2)
+	return head
+}
