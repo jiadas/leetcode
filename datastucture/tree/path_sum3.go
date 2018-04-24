a2a173c3cce7b705d6c0fe1eb6b008e819599a2e
jia <jiada8866@126.com>
2018-04-24 22:49:17 +0800

    feat(solution): 437. Path Sum III

---
+++ datastucture/tree/path_sum3.go
@@ -0,0 +1,22 @@
+package main
+
+func pathSum(root *TreeNode, sum int) int {
+	if root == nil {
+		return 0
+	}
+	// 以 root 为起点，和为 sum 的路径数 + 左子树里和为 sum 的路径数 + 右子树里和为 sum 的路径数
+	return pathSumStartWithRoot(root, sum) + pathSum(root.Left, sum) + pathSum(root.Right, sum)
+}
+
+// pathSumStartWithRoot 计算以 root 为起点，和为 sum 的路径数
+func pathSumStartWithRoot(root *TreeNode, sum int) int {
+	if root == nil {
+		return 0
+	}
+	var ret int
+	if root.Val == sum {
+		ret++
+	}
+	ret += pathSumStartWithRoot(root.Left, sum-root.Val) + pathSumStartWithRoot(root.Right, sum-root.Val)
+	return ret
+}
