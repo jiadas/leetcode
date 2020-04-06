e649ef8eb67fef8907f19d3ac917de8277d54b30
shijiada <jiada8866@126.com>
2020-04-06 21:28:18 +0800

    fix(golang): 35. Search Insert Position

---
+++ golang/35. Search Insert Position.go
@@ -0,0 +1,16 @@
+package golang
+
+func searchInsert(nums []int, target int) int {
+	l, r := 0, len(nums)-1
+	for l <= r {
+		mid := l + (r-l)>>1
+		if nums[mid] > target {
+			r = mid - 1
+		} else if nums[mid] < target {
+			l = mid + 1
+		} else {
+			return mid
+		}
+	}
+	return l
+}
