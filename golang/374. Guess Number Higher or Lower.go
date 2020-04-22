1ed5a15b009790258e25d914f7c26f6daf8eb0f2
shijiada <jiada8866@126.com>
2020-04-22 11:05:50 +0800

    fix(golang): 374. Guess Number Higher or Lower

---
+++ golang/374. Guess Number Higher or Lower.go
@@ -0,0 +1,31 @@
+package golang
+
+/**
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is lower than the guess number
+ *			      1 if num is higher than the guess number
+ *               otherwise return 0
+ * func guess(num int) int;
+ */
+
+func guessNumber(n int) int {
+	left, right := 0, n
+	var mid int
+	for left <= right {
+		mid = left + (right-left)>>1
+		switch guess(mid) {
+		case -1:
+			right = mid - 1
+		case 1:
+			left = mid + 1
+		case 0:
+			return mid
+		}
+	}
+	return -1
+}
+
+func guess(num int) int {
+	return 0
+}
