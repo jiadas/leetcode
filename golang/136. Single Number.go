e1c28557e2d3306bd62d08574b223e158e093673
shijiada <jiada8866@126.com>
2020-05-14 09:49:42 +0800

    feat(golang): 136. Single Number

---
+++ golang/136. Single Number.go
@@ -0,0 +1,9 @@
+package golang
+
+func singleNumber(nums []int) int {
+	var result int
+	for _, num := range nums {
+		result ^= num
+	}
+	return result
+}
---
+++ golang/136. Single Number_test.go
@@ -0,0 +1,36 @@
+package golang
+
+import "testing"
+
+func Test_singleNumber(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "e1",
+			args: args{
+				nums: []int{2, 2, 1},
+			},
+			want: 1,
+		},
+		{
+			name: "e2",
+			args: args{
+				nums: []int{4, 1, 2, 1, 2},
+			},
+			want: 4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := singleNumber(tt.args.nums); got != tt.want {
+				t.Errorf("singleNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
