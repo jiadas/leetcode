14b5a32cbfc2a51d57955f1bf2a2dcd50a79f535
shijiada <jiada8866@126.com>
2020-09-06 19:06:03 +0800

    167. Two Sum II - Input array is sorted

---
+++ golang/167. Two Sum II - Input array is sorted_test.go
@@ -0,0 +1,34 @@
+package golang
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_twoSum2(t *testing.T) {
+	type args struct {
+		numbers []int
+		target  int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "e1",
+			args: args{
+				numbers: []int{2, 7, 9, 11},
+				target:  9,
+			},
+			want: []int{1, 2},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := twoSum2(tt.args.numbers, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("twoSum2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
