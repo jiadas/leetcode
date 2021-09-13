10b1434fa55c236c68e405ccf2d8da049e8d413a
shijiada <jiada8866@126.com>
2021-09-13 10:47:00 +0800

    feat(golang): 237.delete-node-in-a-linked-list

---
+++ plugin/leetcode/editor/cn/237.delete-node-in-a-linked-list.go
@@ -0,0 +1,16 @@
+package golang
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteNode(node *ListNode) {
+	node.Val = node.Next.Val
+	node.Next = node.Next.Next
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
