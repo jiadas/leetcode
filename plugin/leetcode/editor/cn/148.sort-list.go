041197c4e874d26d678cd825237aa229529784fc
shijiada <jiada8866@126.com>
2021-09-13 10:46:38 +0800

    feat(golang): 148.sort-list

---
+++ plugin/leetcode/editor/cn/148.sort-list.go
@@ -0,0 +1,58 @@
+package golang
+
+// leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func sortList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	mid := findMiddleNode(head)
+	rightHead := mid.Next
+	mid.Next = nil
+	return mergeTwoLists(sortList(head), sortList(rightHead))
+}
+
+func findMiddleNode(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast.Next != nil && fast.Next.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return slow
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+	dummy := &ListNode{}
+	curr := dummy
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			curr.Next = l1
+			l1 = l1.Next
+		} else {
+			curr.Next = l2
+			l2 = l2.Next
+		}
+		curr = curr.Next
+	}
+	if l1 == nil {
+		curr.Next = l2
+	}
+	if l2 == nil {
+		curr.Next = l1
+	}
+	return dummy.Next
+}
+
+// leetcode submit region end(Prohibit modification and deletion)
