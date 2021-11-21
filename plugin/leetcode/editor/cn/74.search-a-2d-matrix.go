f8465e552ab2c9371c336277db36835494035acc
shijiada <jiada8866@126.com>
2021-11-21 22:21:12 +0800

    feat(golang): 74.search-a-2d-matrix

---
+++ plugin/leetcode/editor/cn/74.search-a-2d-matrix.go
@@ -0,0 +1,18 @@
+package golang
+
+import "sort"
+
+// leetcode submit region begin(Prohibit modification and deletion)
+
+func searchMatrix(matrix [][]int, target int) bool {
+	row := sort.Search(len(matrix), func(i int) bool {
+		return matrix[i][0] > target
+	}) - 1
+	if row < 0 {
+		return false
+	}
+	col := sort.SearchInts(matrix[row], target)
+	return col < len(matrix[row]) && matrix[row][col] == target
+}
+
+// leetcode submit region end(Prohibit modification and deletion)
