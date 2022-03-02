2d3270b621f4409eed336091a6fe0d5fc0c7aab4
shijiada <jiada8866@126.com>
2022-03-02 14:37:15 +0800

    876.middle-of-the-linked-list

---
+++ plugin/leetcode/editor/cn/876.middle-of-the-linked-list.go
@@ -0,0 +1,20 @@
+package golang
+
+// leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func middleNode(head *ListNode) *ListNode {
+	fast, slow := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return slow
+}
+
+// leetcode submit region end(Prohibit modification and deletion)
