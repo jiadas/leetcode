6dd58991ac93aaa3cbc5f07ec120595a5c7b8cd1
shijiada <jiada8866@126.com>
2022-03-17 10:30:23 +0800

    226.invert-binary-tree

---
+++ plugin/leetcode/editor/cn/226.invert-binary-tree.go
@@ -0,0 +1,22 @@
+package golang
+
+// leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+	root.Left, root.Right = root.Right, root.Left
+	invertTree(root.Left)
+	invertTree(root.Right)
+	return root
+}
+
+// leetcode submit region end(Prohibit modification and deletion)
