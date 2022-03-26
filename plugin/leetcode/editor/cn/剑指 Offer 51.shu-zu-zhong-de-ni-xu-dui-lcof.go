a2aa9e4e54c601ff196c5b9e3c76474ea24209d2
shijiada <jiada8866@126.com>
2022-03-27 01:02:00 +0800

    剑指 Offer 51.shu-zu-zhong-de-ni-xu-dui-lcof

---
+++ plugin/leetcode/editor/cn/剑指 Offer 51.shu-zu-zhong-de-ni-xu-dui-lcof.go
@@ -0,0 +1,54 @@
+package golang
+
+// leetcode submit region begin(Prohibit modification and deletion)
+func reversePairs(nums []int) int {
+	_, ans := reversePairsCore(nums)
+	return ans
+}
+
+func reversePairsCore(nums []int) ([]int, int) {
+	if len(nums) <= 1 {
+		return nums, 0
+	}
+
+	mid := len(nums) >> 1
+	sortedLeft, lc := reversePairsCore(nums[:mid])
+	sortedRight, rc := reversePairsCore(nums[mid:])
+
+	var (
+		sortedLen   = len(sortedLeft) + len(sortedRight)
+		sortedIndex = sortedLen - 1
+		sorted      = make([]int, sortedLen)
+		count       int
+	)
+
+	// bug1: l 和 r 从 len 开始，而不是 len-1
+	l, r := len(sortedLeft)-1, len(sortedRight)-1
+	for l >= 0 && r >= 0 {
+		if sortedLeft[l] > sortedRight[r] {
+			count += r + 1
+			sorted[sortedIndex] = sortedLeft[l]
+			l--
+		} else {
+			sorted[sortedIndex] = sortedRight[r]
+			r--
+		}
+		sortedIndex--
+	}
+
+	// bug2: 直接用了 for i:=0; i<=l; i++{} 来将剩余的元素用 sorted[sortedIndex] 加到 sorted 里，
+	// 而忽略了 sortedIndex 是从后往前遍历的，i 则是从前往后遍历的
+	for ; l >= 0; l-- {
+		sorted[sortedIndex] = sortedLeft[l]
+		sortedIndex--
+	}
+
+	for ; r >= 0; r-- {
+		sorted[sortedIndex] = sortedRight[r]
+		sortedIndex--
+	}
+
+	return sorted, lc + rc + count
+}
+
+// leetcode submit region end(Prohibit modification and deletion)
---
+++ plugin/leetcode/editor/cn/剑指 Offer 51.shu-zu-zhong-de-ni-xu-dui-lcof_test.go
@@ -0,0 +1,36 @@
+package golang
+
+import "testing"
+
+func Test_reversePairs(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "e1",
+			args: args{
+				nums: []int{7, 5, 6, 4},
+			},
+			want: 5,
+		},
+		{
+			name: "wa1",
+			args: args{
+				nums: []int{233, 2000000001, 234, 2000000006, 235, 2000000003, 236, 2000000007, 237, 2000000002, 2000000005, 233, 233, 233, 233, 233, 2000000004},
+			},
+			want: 69,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reversePairs(tt.args.nums); got != tt.want {
+				t.Errorf("reversePairs() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
