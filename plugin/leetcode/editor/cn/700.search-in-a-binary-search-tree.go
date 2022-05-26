4f2c314b0af7ec036dee13e0c7aa6bbb1df2b4e3
shijiada <jiada8866@126.com>
2022-05-27 00:14:38 +0800

    700.search-in-a-binary-search-tree

---
+++ plugin/leetcode/editor/cn/700.search-in-a-binary-search-tree.go
@@ -0,0 +1,22 @@
+package golang
+
+// leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root == nil || root.Val == val {
+		return root
+	}
+	if root.Val > val {
+		return searchBST(root.Left, val)
+	}
+	return searchBST(root.Right, val)
+}
+
+// leetcode submit region end(Prohibit modification and deletion)
