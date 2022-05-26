442df2d92eb90a5c902bb68384ce921476abda0a
shijiada <jiada8866@126.com>
2022-05-27 00:46:24 +0800

    701.insert-into-a-binary-search-tree

---
+++ plugin/leetcode/editor/cn/701.insert-into-a-binary-search-tree.go
@@ -0,0 +1,25 @@
+package golang
+
+// leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func insertIntoBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return &TreeNode{Val: val}
+	}
+	if root.Val > val {
+		root.Left = insertIntoBST(root.Left, val)
+	}
+	if root.Val < val {
+		root.Right = insertIntoBST(root.Right, val)
+	}
+	return root
+}
+
+// leetcode submit region end(Prohibit modification and deletion)
