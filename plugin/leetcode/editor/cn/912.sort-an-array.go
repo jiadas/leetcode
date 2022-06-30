5f2182dc3cbd32dcc8c159fc6abdd18571316e58
shijiada <jiada8866@126.com>
2022-06-30 23:38:10 +0800

    heap sort

--- plugin/leetcode/editor/cn/912.sort-an-array.go
+++ plugin/leetcode/editor/cn/912.sort-an-array.go
@@ -7,9 +7,10 @@ import (
 
 // leetcode submit region begin(Prohibit modification and deletion)
 func sortArray(nums []int) []int {
-	return doRandomizedQuickSort(nums)
+	return heapSort(nums)
 }
 
+// 合并排序
 func mergeSort(nums []int) []int {
 	n := len(nums)
 	if n <= 1 {
@@ -33,6 +34,7 @@ func mergeSort(nums []int) []int {
 	return nums
 }
 
+// 快速排序
 func doRandomizedQuickSort(nums []int) []int {
 	rand.Seed(time.Now().UnixNano())
 	randomizedQuickSort(nums, 0, len(nums)-1)
@@ -55,6 +57,42 @@ func randomizedQuickSort(nums []int, l, r int) {
 		nums[i], nums[r] = nums[r], nums[i]
 		randomizedQuickSort(nums, l, i-1)
 		randomizedQuickSort(nums, i+1, r)
+	}
+}
+
+// 堆排序
+func heapSort(nums []int) []int {
+	initHeap(nums)
+	for i := len(nums) - 1; i > 0; i-- {
+		nums[i], nums[0] = nums[0], nums[i]
+		down(nums, 0, i)
+	}
+
+	return nums
+}
+
+func initHeap(nums []int) {
+	n := len(nums)
+	for i := n/2 - 1; i >= 0; i-- {
+		down(nums, i, n)
+	}
+}
+
+func down(nums []int, i, n int) {
+	for {
+		j1 := 2*i + 1
+		if j1 >= n || j1 < 0 {
+			break
+		}
+		j := j1 // left child
+		if j2 := j1 + 1; j2 < n && nums[j1] < nums[j2] {
+			j = j2 // right child
+		}
+		if nums[j] <= nums[i] {
+			break
+		}
+		nums[i], nums[j] = nums[j], nums[i]
+		i = j
 	}
 }
 
