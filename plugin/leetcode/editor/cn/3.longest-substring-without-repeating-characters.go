8d648cd90010326d633ca4014b6f2b4d67782fb2
shijiada <jiada8866@126.com>
2022-09-01 10:21:00 +0800

    chore: 3.longest-substring-without-repeating-characters

--- plugin/leetcode/editor/cn/3.longest-substring-without-repeating-characters.go
+++ plugin/leetcode/editor/cn/3.longest-substring-without-repeating-characters.go
@@ -7,10 +7,12 @@ func lengthOfLongestSubstring(s string) int {
 	window := map[byte]int{}
 	for l, r := 0, 0; r < len(s); r++ {
 		window[s[r]]++
+		// 当 window[s[r]] 值大于 1 时，说明窗口中存在重复字符，不符合条件，就该移动 left 缩小窗口了
 		for window[s[r]] > 1 {
 			window[s[l]]--
 			l++
 		}
+		// 要在收缩窗口完成后更新 ans，因为窗口收缩的 for 条件是存在重复元素，换句话说收缩完成后一定保证窗口中没有重复
 		ans = maxInt(ans, r-l+1)
 	}
 	return ans
